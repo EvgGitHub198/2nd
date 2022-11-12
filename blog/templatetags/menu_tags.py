bc3baa928872a3d2d5b500e6ba3adba55f0f0e8d
EvgGitHub198 <mishagin02@mail.ru>
2022-11-13 01:10:44 +0300

    4commit

--- blog/templatetags/menu_tags.py
+++ blog/templatetags/menu_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Category
+from blog.models import Category, Post
 
 register = template.Library()
 
@@ -9,7 +9,15 @@ register = template.Library()
 #def get_categories():
 #    return
 
+
+
 @register.inclusion_tag('blog/include/tags/top_menu.html')
 def get_categories():
-    category = Category.objects.filter(parent__isnull=True).order_by('name')
+    category = Category.objects.all()
     return {'list_category': category}
+
+
+@register.inclusion_tag('blog/include/tags/recipes_tag.html')
+def get_last_posts():
+    posts = Post.objects.select_related('category').order_by('-id')[:5]
+    return {'list_last_post': posts}
